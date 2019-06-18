e5b4dd79d1ab0532fce1e948b68b431d46b8dbb1
unknown <katiajaafar28@gmail.com>
2019-06-18 11:36:30 +0300

    emit newMessage on reateMessage

--- server/server.js
+++ server/server.js
@@ -16,6 +16,31 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('new user connected');
 
+	socket.emit('newEmail', {
+		from: "test@test.com",
+		text: "what's up?",
+		createdAt: 123
+	});
+
+	socket.emit('newMessage', {
+		from: "admin@test.com",
+		text: "Open it",
+		createdAt: 234
+	})
+
+	socket.on('createEmail', (newEmail) => {
+        console.log('createEmail', newEmail);
+	})
+
+	socket.on('createMessage', (message) => {
+		console.log('createMsg', message);
+		io.emit('newMessage', {
+			from: message.from,
+		    text: message.text,
+		    createdAt: new Date().getTime()
+		})
+	})
+
 	socket.on('disconnect', () => {
     	console.log('disconnected from the server');
     })
