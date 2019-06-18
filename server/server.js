52acfd684cd2b566e355ef4b8f88f4c858a66eb2
unknown <katiajaafar28@gmail.com>
2019-06-18 12:45:30 +0300

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,9 +16,28 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('new user connected');
 
+	socket.emit('newMessage', {
+			from: "admin",
+			text: "welcome",
+			createdAt: new Date().getTime()
+		})
+
+	socket.broadcast.emit('newMessage', {
+			from: 'admin',
+		    text: 'New user joined',
+		    createdAt: new Date().getTime()
+		})
+		
+
 	socket.on('createMessage', (message) => {
 		console.log('createMsg', message);
-		io.emit('newMessage', {
+		/*io.emit('newMessage', {
+			from: message.from,
+		    text: message.text,
+		    createdAt: new Date().getTime()
+		})*/
+
+		socket.broadcast.emit('newMessage', {
 			from: message.from,
 		    text: message.text,
 		    createdAt: new Date().getTime()
