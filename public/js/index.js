ba3e2283a09ff32b1f5cc8154f66c5a4b22f2edf
unknown <katiajaafar28@gmail.com>
2019-06-19 16:19:53 +0300

    added formatted time

--- public/js/index.js
+++ public/js/index.js
@@ -9,17 +9,18 @@ var socket = io();
     })
 
     socket.on('newMessage', function(msg) {
-    	console.log('new msg', msg);
+    	var formattedTime = moment(msg.createdAt).format('h:mm a');
         var li = $('<li></li>');
-            li.text(`${msg.from}: ${msg.text}`) ;
+            li.text(`${msg.from} ${formattedTime}: ${msg.text}`) ;
 
             $('#messages').append(li);
     })
 
     socket.on('newLocationMessage', function(msg) {
+        var formattedTime = moment(msg.createdAt).format('h:mm a');
         var li = $('<li></li>');
         var a = $('<a target="_blank">My current location</a>');
-            li.text(`${msg.from}: `);
+            li.text(`${msg.from} ${formattedTime}: `);
             a.attr('href', msg.url);
             li.append(a);
             $('#messages').append(li);
@@ -38,23 +39,29 @@ $('#message-form').on('submit', function(e) {
     socket.emit('createMessage', {
         from: 'User',
         text: $('[name=message]').val()
-    }, function(data) {
-        console.log('got it ', data);
+    }, function() {
+        $('[name=message]').val('');
     })
 })
 
 var locationButton = $('#send-location');
+
 locationButton.on('click', function() {
     if(!navigator.geolocation) {
         return alert('Geolocation not supported by your browser');
     }
+    
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
 
     navigator.geolocation.getCurrentPosition(function(position) {
+       locationButton.removeAttr('disabled').text('Send location');
+
        socket.emit('createLocationMessage', {
           lat: position.coords.latitude,
           lon: position.coords.longitude
        })
     }, function() {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch position');
     });
 })
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,6 +1,8 @@
+const moment = require('moment');
+
 const generateMessage = (from, text) => {
   return {
-  	from, text, createdAt: new Date().getTime()
+  	from, text, createdAt: moment().valueOf()
   }
 }
 
@@ -8,7 +10,7 @@ const generateLocationMessage = (from, lat, lon) => {
 	return {
 		from, 
 		url: `https://www.google.com/maps?q=${lat},${lon}`,
-		createdAt: new Date().getTime()
+		createdAt: moment().valueOf()
 	}
 }
 
